79e531a52d702ddcd675cf7afb8ef2d6414f98cc
goranaviani <goranaviani@gmail.com>
2020-02-06 16:09:56 +0100

    logging debug and info levels

--- python_logging.py
+++ python_logging.py
@@ -1,7 +1,17 @@
 import logging
 
-logging.debug('This is a debug message')
-logging.info('This is an info message')
+#basic terminal logging
+#logging.debug('This is a debug message')
+#logging.info('This is an info message')
+#logging.warning('This is a warning message')
+#logging.error('This is an error message')
+#logging.critical('This is a critical message')
+
+
+#increasing loging level to include debug and info
+logging.basicConfig(level=logging.DEBUG)
+logging.debug('This will get logged')
+logging.info('This will also get loged now')
 logging.warning('This is a warning message')
 logging.error('This is an error message')
 logging.critical('This is a critical message')
