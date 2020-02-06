c7ec55810b5bc7919f4195112a56ffcaee90bff0
goranaviani <goranaviani@gmail.com>
2020-02-06 16:11:26 +0100

    basic logging to a file

--- python_logging.py
+++ python_logging.py
@@ -9,9 +9,15 @@ import logging
 
 
 #increasing loging level to include debug and info
+'''
 logging.basicConfig(level=logging.DEBUG)
 logging.debug('This will get logged')
 logging.info('This will also get loged now')
 logging.warning('This is a warning message')
 logging.error('This is an error message')
 logging.critical('This is a critical message')
+'''
+
+#basic logging to a file
+logging.basicConfig(filename='logging_file.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+logging.warning('This will get logged to a file')
